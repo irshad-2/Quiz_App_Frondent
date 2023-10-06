161ac6712c191ce3d6f2e99bf891044bb1b36814
Irshad-2 <irshadsajeena301@gmail.com>
2023-10-06 09:59:15 +0530

    singup updating

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import './App.css';
+import "./App.css";
+// import { createContext, useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MainPage from './components/screens/MainPage';
-import React, { useRef } from 'react';
+import MainPage from "./components/screens/MainPage";
+import NoMatch from "./components/screens/NoMatch";
+import LogIn from "./components/screens/LogIn";
+import SignUp from "./components/screens/SignUp";
+import Category from "./components/screens/Category";
+import Store from "./components/context/store";
 
-
+// export const UserContext = createContext();
 
 function App() {
-  return (
-   <>
-      <store>
-        <Router>
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-          </Routes>
-        </Router>
-      </store>
-   
-   
-   </>
-  );
+
+    return (
+        <>
+            <Store>
+                <Router>
+                    <Routes>
+                        <Route path="/home/:category" element={<MainPage />} />
+                        <Route path="/category" element={<Category />} />
+                        <Route path="/login" element={<LogIn />} />
+                        <Route path="/signup/" element={<SignUp />} />
+                        <Route path="*" element={<NoMatch />} />
+                    </Routes>
+                </Router>
+            </Store>
+        </>
+    );
 }
 
 export default App;
